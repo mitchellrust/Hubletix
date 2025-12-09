29b5c7d86c50251e43902c401454e2dd0f6d1888
Mitchell Rust <mitchellrust@gmail.com>
2025-12-08 22:21:06 -0700

    Migration for membership plan attributes

---
+++ src/ClubManagement.Infrastructure/Migrations/App/20251209051917_MembershipAnnotations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClubManagement.Infrastructure.Migrations.App
+{
+    /// <inheritdoc />
+    public partial class MembershipAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "MembershipPlans",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "MembershipPlans",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+    }
+}
